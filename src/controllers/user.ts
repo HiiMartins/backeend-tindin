1f796b5aab1cc84904b6d7bee9dd2371d8efcd4b
HiiMartins <higor.dsm10@gmail.com>
2022-02-27 07:12:12 -0300

    implementation post controller in user and you test cases

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,23 @@
 import { Controller, Post } from "@overnightjs/core";
+import { User } from "@src/model/user";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
 @Controller('users')
 export class UserController {
 
     @Post('')
     public async createUser(req: Request, res: Response): Promise<void> {
-        res.status(201).send( { ...req.body, id: 'fake_id' });
+        try{
+            const user = new User(req.body);
+            const result = await user.save();
+            res.status(201).send(result);
+        } catch (error) {
+            if (error instanceof mongoose.Error.ValidationError){
+                res.status(422).send({ error: 'validation error' });
+            } else {
+                res.status(500).send({ error: 'Internal Server Error' });
+            }
+        }
     }
 }
--- test/functional/user.test.ts
+++ test/functional/user.test.ts
@@ -1,13 +1,37 @@
-describe('User creation test', () => {
-    it('should create user with success, phase green of TDD ', async() => {
-        const newUser = {
-            name: 'Higor Martins',
-            email: 'higor@teste.com',
-            password: '123'
-        };
+import { User } from "@src/model/user";
+
+describe('Users functional tests', () => {
+    beforeAll(async () => await User.deleteMany({}));
+    describe('When creating a new User', () => {
+        it('should create a user with success', async() => {
+            const newUser = {
+                name: 'Higor Martins',
+                email: 'higor@teste.com',
+                password: '123'
+            }
         
-        const response = await global.testRequest.post('/users').send(newUser);
-        expect(response.status).toBe(201);
-        expect(response.body).toEqual(expect.objectContaining(newUser));
+            const response = await global.testRequest.post('/users').send(newUser);
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual(expect.objectContaining(newUser));
+        });
+
+        it('should return 422 when there is a validation error', async () => {
+            const newUser = {
+                name: null,
+                email: 'higor@teste.com.br',
+                password: '123'
+            };
+
+            const response = await await global.testRequest.post('/users').send(newUser);
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                error:
+                    'validation error',
+            });    
+        });
+
+        it.skip('should return 500 when there is any error than validation error', async() => {
+            //TODO think in a way to throw a 500
+        })
     });
-});
+});  
