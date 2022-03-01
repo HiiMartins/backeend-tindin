515481e25afea0b8437c188ee9b7ba401d1cb549
HiiMartins <higor.dsm10@gmail.com>
2022-03-01 15:59:52 -0300

    added in the post method of Class and in its test, validation error handling

--- src/controllers/class.ts
+++ src/controllers/class.ts
@@ -1,19 +1,28 @@
 import { Controller, Get, Post } from "@overnightjs/core";
 import { Class } from "@src/model/class";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
 @Controller('classes')
 export class ClassController {
 
     @Post('')
     public async createClass(req: Request, res: Response) {
-        const newClass = new Class(req.body);
-        const result = await newClass.save();
-        res.status(201).send(result);
+        try {
+            const newClass = new Class(req.body);
+            const result = await newClass.save();
+            res.status(201).send(result);
+        } catch (error){
+            if (error instanceof mongoose.Error.ValidationError){
+                res.status(422).send({ error: error.message });
+            } else {
+                res.status(500).send({ error: 'Internal Server Error' });
+            }
+        }
     }
 
     @Get('')
     public listClasses(req: Request, res: Response) {
-        res.send({message: 'classes list'});
+        res.send({ message: 'classes list' });
     }
 }
--- test/functional/class.test.ts
+++ test/functional/class.test.ts
@@ -3,7 +3,7 @@ import { Class } from "@src/model/class";
 describe('Classes functional test', () => {
     beforeAll(async () => await Class.deleteMany({}));
     describe('Implementation of tests for the post method', () => {
-        it('should create a class', async () => {
+        it('should create a class with success', async () => {
             const newClass = {
                 name: 'git básico',
                 description: 'introcução ao git',
@@ -18,13 +18,33 @@ describe('Classes functional test', () => {
             expect(response.status).toBe(201);
             expect(response.body).toEqual(expect.objectContaining(newClass));
         });
+
+        it('should return 422 when there is a validation error', async () => {
+            const newClass = {
+                name: 'git básico',
+                description: 'introcução ao git',
+                video: 'www.link-aula.com',
+                dateInit: '2021-09-10T03:00:00.000Z',
+                dateEnd: '2022-01-30T03:00:00.000Z',
+                dateCreated: '2021-08-30T03:00:00.000Z',
+                totalComments: 'Invalid_string'
+            };
+
+            const response = await global.testRequest.post('/classes').send(newClass);
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                error:
+                    'Class validation failed: totalComments: Cast to Number failed for value "Invalid_string" (type string) at path "totalComments"'
+            });
+        });
+
     });
-    describe('Implementation of tests for the get method', () =>{
-        it('should return a list of classes', async () => {
+    describe('Implementation of tests for the get method', () => {
+        it('should return a list of classes with success', async () => {
 
             const response = await global.testRequest.get('/classes');
             expect(response.status).toBe(200);
-            expect(response.body).toEqual({message:'classes list'});
+            expect(response.body).toEqual({ message: 'classes list' });
         });
     });
 });
